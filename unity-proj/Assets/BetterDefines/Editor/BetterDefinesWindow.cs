6ae4296fe9db7fa53d50247aa0558e528177e66f
Taras Leskiv <leskiv.taras@gmail.com>
2016-03-07 23:22:36 +0100

    started

--- unity-proj/Assets/BetterDefines/Editor/BetterDefinesWindow.cs
+++ unity-proj/Assets/BetterDefines/Editor/BetterDefinesWindow.cs
@@ -1,15 +1,21 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using UnityEditor;
 
-public class BetterDefinesWindow : MonoBehaviour {
+public class BetterDefinesWindow : EditorWindow
+{
+    [MenuItem("Window/Better Defines")]
+    private static void Init()
+    {
+        var window = (BetterDefinesWindow) GetWindow(typeof (BetterDefinesWindow));
+        window.Show();
+    }
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void OnGUI()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            EditorGUILayout.BeginHorizontal("box");
+            EditorGUILayout.LabelField("Label");
+            EditorGUILayout.EndHorizontal();
+        }
+    }
 }
