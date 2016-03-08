a9e88c5b8222d759b53fa915eb48e3df6df35752
Taras Leskiv <taras@getsocial.im>
2016-03-08 14:44:01 +0100

    toggle defines method added

---
+++ unity-proj/Assets/BetterDefines/Editor/BetterDefinesUtils.cs
@@ -0,0 +1,36 @@
+﻿using UnityEditor;
+using System.Collections.Generic;
+
+public static class BetterDefinesUtils
+{
+    private static void ToggleFlag(string targetFlag, bool enable, params BuildTargetGroup[] supportedPlatforms)
+    {
+        foreach (var targetPlatform in supportedPlatforms)
+        {
+            var scriptDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(targetPlatform);
+            var flags = new List<string>(scriptDefines.Split(';'));
+
+            if (flags.Contains(targetFlag))
+            {
+                if (!enable)
+                {
+                    flags.Remove(targetFlag);
+                }
+            }
+            else
+            {
+                if (enable)
+                {
+                    flags.Add(targetFlag);
+                }
+            }
+
+            var result = string.Join(";", flags.ToArray());
+
+            if (scriptDefines != result)
+            {
+                PlayerSettings.SetScriptingDefineSymbolsForGroup(targetPlatform, result);
+            }
+        }
+    }
+}
